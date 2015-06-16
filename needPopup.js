76ed3e1ec9c3d81a5255843ef5131ca9c6167ddc
Dzmitry Vasilevsky <dzm.vasilevsky@gmail.com>
2015-06-16 16:45:34 +0300

    update 1

--- needPopup.js
+++ needPopup.js
@@ -42,7 +42,7 @@ var needPopup = (function() {
 			})
 
 			// bind popup hide to removers
-			$(popup.body).on('click','.needpopup_wrapper .close', function(event) {
+			$(popup.body).on('click','.needpopup_wrapper .remove, .needpopup_remover', function(event) {
 				event.preventDefault();
 				needPopup.hide();
 			})
@@ -52,9 +52,9 @@ var needPopup = (function() {
 				event.preventDefault();
 				event.stopPropagation();
 				if (!popup.options.closeOnOutside) return;
-				
+
 				// check if clicked outside of popup window
-				if ($(event.target).closest('.needpopup-opened').length || $(event.target).is('.needpopup-opened, .close')) return;
+				if ($(event.target).closest('.needpopup-opened').length || $(event.target).is('.needpopup-opened, .remove')) return;
 
 				needPopup.hide();
 			})
@@ -67,13 +67,24 @@ var needPopup = (function() {
 			})
 
 			// corrections on window resize
+			popup.resizeTimeout = 0;
+			popup.resizeAllowed = true;
 			$(popup.window).on('resize',function() {
-				// centrify popup
-				needPopup.centrify();
-				// recalculate window scroll height
-				popup.scrollHeight = popup.body.scrollHeight > popup.html.scrollHeight ? popup.body.scrollHeight : popup.html.scrollHeight,
-				// change open htmml class if needed
-		 		popup.openHtmlClass = popup.scrollHeight > popup.window.innerHeight ? 'needpopup-opened needpopup-scrolled' : 'needpopup-opened';
+				// throttling
+				clearTimeout(popup.resizeTimeout);
+				if (popup.resizeAllowed) {
+          popup.resizeAllowed = false;
+
+					// centrify popup
+					needPopup.centrify();
+					// recalculate window scroll height
+					popup.scrollHeight = popup.body.scrollHeight > popup.html.scrollHeight ? popup.body.scrollHeight : popup.html.scrollHeight,
+					// change open htmml class if needed
+			 		popup.openHtmlClass = popup.scrollHeight > popup.window.innerHeight ? 'needpopup-opened needpopup-scrolled' : 'needpopup-opened';
+			 	}
+        popup.resizeTimeout = setTimeout(function() {
+          popup.resizeAllowed = true;
+        }, 100);
 			})
 
 			// create popup wrapper
@@ -97,6 +108,9 @@ var needPopup = (function() {
 			if (!!popup.target.data('needpopupOptions'))
 				popup.options = needPopup.config[popup.target.data('needpopupOptions')];
 
+			// cache popup width
+			popup.minWidth = popup.target.outerWidth();
+
 			// recalculate window scroll top
 			popup.scrollTopVal = popup.window.pageYOffset;
 
@@ -107,9 +121,9 @@ var needPopup = (function() {
 			// create layout
 			popup.wrapper.append(popup.target);
 			if (popup.options.removerPlace == 'outside')
-				popup.target.after('<a href="#" class="close needpopup_remover">×</a>');
+				popup.wrapper.after('<a href="#" class="needpopup_remover">×</a>');
 			else if (popup.options.removerPlace == 'inside')
-				popup.target.append('<a href="#" class="close needpopup_remover">×</a>');
+				popup.target.append('<a href="#" class="needpopup_remover">×</a>');
 			
 			// display popup
 			popup.target.show();
@@ -130,14 +144,14 @@ var needPopup = (function() {
 			// hide popup and unset property
 			popup.target.hide().removeClass('opened');
 			popup.target = 0;
-			// remove close
+			// delete remover
 			$('.needpopup_remover').remove();
 
 			// full hiding
 			if (!_partial) {
 
 				// unblock page scroll
-				$(popup.html).removeClass(popup.openHtmlClass);
+				$(popup.html).removeClass(popup.openHtmlClass).removeClass('needpopup-overflow');
 				$(popup.body).css({'top': 0}).scrollTop(popup.scrollTopVal);
 				$(popup.html).scrollTop(popup.scrollTopVal);
 			}
@@ -148,11 +162,21 @@ var needPopup = (function() {
 
 		// centrify popup
 		centrify: function() {
-			if (popup.target)
+			if (popup.target) {
+				// vertical centrification
 				if (popup.target.outerHeight() > popup.window.innerHeight)
-					popup.target.addClass('stacked').css({'margin-top':'0', 'top':'0'});
+					popup.target.addClass('stacked');
 				else
 					popup.target.removeClass('stacked').css({'margin-top':-popup.target.outerHeight()/2, 'top':'50%'});
+
+				// detect width overflow
+				popup.minWidth = $(popup.html).hasClass('needpopup-overflow') ? popup.minWidth : popup.target.outerWidth();
+				if (popup.minWidth + 30 >= popup.window.innerWidth) {
+					$(popup.html).addClass('needpopup-overflow');
+				} else {
+					$(popup.html).removeClass('needpopup-overflow');
+				}
+			}
 		},
 
 		// configuration object which contains all options sets
